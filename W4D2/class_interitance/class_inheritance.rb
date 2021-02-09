9238da16e615e9ab791e844ca7275548406a6735
frmendez21 <frmendez209@gmail.com>
2021-02-09 13:51:37 -0800

    almost complete

--- W4D2/class_interitance/class_inheritance.rb
+++ W4D2/class_interitance/class_inheritance.rb
@@ -1,2 +1,33 @@
-class 
+class Employee
+
+    attr_reader :name, :title, :salary, :boss
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary 
+        @boss = boss  
+    end
+
+    def bonus(multiplier)
+        @salary * multiplier 
+    end
+
+end
     
+class Manager < Employee
+
+    def initialize(name, title, salary, boss, employees)
+        super(name, title, salary, boss) 
+        @employees = employees  
+    end
+
+    def bonus(multiplier) 
+        bonus = 0
+        @employees.each do |employee|
+            bonus += empolyee.bonus(multiplier)
+        end
+        bonus * multiplier
+    end
+
+end
