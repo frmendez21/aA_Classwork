d920f3f72339f3350509d328a99caf6dfcf028ad
frmendez21 <frmendez209@gmail.com>
2021-02-08 17:42:50 -0800

    almost complete

--- W4D1/knights_travails/knightpath.rb
+++ W4D1/knights_travails/knightpath.rb
@@ -83,16 +83,5 @@ class KnightPathFinder
         end
        return queues
     end
-
-
-
-
-
-
-
-
 end 
 
-kpf = KnightPathFinder.new([0, 0])
-kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
-p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
