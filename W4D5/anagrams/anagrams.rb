e48bcae71f3e936870200e80cdea02c380296a17
Satomi Dean <satomiampuku2@gmail.com>
2021-02-12 15:12:14 -0800

    anagram done!

--- W4D5/anagrams/anagrams.rb
+++ W4D5/anagrams/anagrams.rb
@@ -21,7 +21,7 @@ def second_anagram?(str1, str2)
   array.empty?
 end
 
-p second_anagram?("gizmo", "sally")   #time -->O(n)  space --> O(1)
+p second_anagram?("gizmo", "sally")   #time -->O(n^2) (becase of the index_find)  space --> O(n)
 p second_anagram?("elvis", "lives")
 
 def third_anagram?(str1, str2)
@@ -30,5 +30,38 @@ def third_anagram?(str1, str2)
   sorted1 == sorted2
 end
 
-p third_anagram?("gizmo", "sally")  #time --> O(n^2) space --> O(1)
+p third_anagram?("gizmo", "sally")  #time worst--> O(n^2)  time best -->O(n log n) space --> O(n)
 p third_anagram?("elvis", "lives")
+
+
+def forth_anagram?(str1, str2)
+  return false if str1.length != str2.length
+
+  hash1 , hash2 = Hash.new(0), Hash.new(0)
+  (0...str1.length).each do |i| 
+    hash1[str1[i]] += 1
+    hash2[str2[i]] += 1
+  end
+  hash1.each {|k, v| return false if !hash2.keys.include?(k) || hash2[k] != v}
+  true
+end
+
+p forth_anagram?("gizmo", "sally")  #time --> O(n) space --> O(1)
+#hash is costant because key is alphabet
+p forth_anagram?("elvis", "lives")
+
+#hash {'g'=>1, 'i' => 1, 'm'=>1, 'o'=>1}
+#str2 = 'sally'
+#iterate over the str1 add the value to hash
+#sort str1 & str2, iterate over str1, if str1[i] != str2[i], false after true
+#if g !=m n 
+def fifth_anagram?(str1, str2)
+  return false if str1.length != str2.length
+  hash1 = {}
+  str1.each_char {|ch| hash1[ch] = 1} 
+  str2.each_char {|ch| return false if !hash1.keys.include?(ch)} 
+  true
+end
+
+p fifth_anagram?("gizmo", "sally")  
+p fifth_anagram?("elvis", "lives")
