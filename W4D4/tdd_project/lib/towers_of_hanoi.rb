625df577897018d490c59f722462846baa2d79e9
Shane <shane@Shanes-iMac.Home>
2021-02-11 19:17:12 -0500

    finishes game

--- W4D4/tdd_project/lib/towers_of_hanoi.rb
+++ W4D4/tdd_project/lib/towers_of_hanoi.rb
@@ -26,14 +26,43 @@ class TowerOfHanoi
     end_stack
   end
 
+  def play
+    while game_over? == false
+      print @stack1
+      print @stack2 
+      print @stack3
+      start = nil
+      last = nil
+      puts "Enter the stack number you wish to take from"
+      input = gets.chomp.to_i
+      puts "Enter the stack number you wish to add to."
+      input2 = gets.chomp.to_i
+      if input == 1
+        start = @stack1
+      elsif input == 2
+        start = @stack2
+      elsif input == 3
+        start = @stack3
+      end
+      if input2 == 1
+        last = @stack1
+      elsif input2 == 2
+        last = @stack2
+      elsif input2 == 3
+        last = @stack3
+      end
 
 
-
-
-
-
-
-
+      self.move(start, last)
+    end
+  end
+  
 
 
 end
+
+# game = TowerOfHanoi.new
+
+# game.play
+
+
