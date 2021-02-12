7823f8c4eec9807f0bafde24f70b1f2fbf6f3750
frmendez21 <frmendez209@gmail.com>
2021-02-12 14:21:56 -0800

    third anagram complete

--- W4D5/anagrams/anagrams.rb
+++ W4D5/anagrams/anagrams.rb
@@ -11,6 +11,24 @@ p first_anagram?("gi", "sa")   #time -->O(n!)  space --> O(n!)
 p first_anagram?("elvis", "lives")
 
 
-def second_anagram?(str)
-
+def second_anagram?(str1, str2)
+  array = str2.split('')
+  str1.each_char do |c|
+    if array.find_index(c) 
+        array.delete(c)
+    end
+  end
+  array.empty?
 end
+
+p second_anagram?("gizmo", "sally")   #time -->O(n)  space --> O(1)
+p second_anagram?("elvis", "lives")
+
+def third_anagram?(str1, str2)
+  sorted1 = str1.split('').sort
+  sorted2 = str2.split('').sort
+  sorted1 == sorted2
+end
+
+p third_anagram?("gizmo", "sally")  #time --> O(n^2) space --> O(1)
+p third_anagram?("elvis", "lives")
