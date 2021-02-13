8394d265ceab883189fc8f60029f8f6b284465a4
Satomi Dean <satomiampuku2@gmail.com>
2021-02-12 17:08:44 -0800

    two_sum almost done!

--- W4D5/two_sum/two_sum.rb
+++ W4D5/two_sum/two_sum.rb
@@ -12,6 +12,34 @@ p bad_two_sum?(arr, 6) # => time -- O(n^2) space -- O(1)
 p bad_two_sum?(arr, 10) 
 
 def okay_two_sum?(arr, target_sum)
-    array = array.sort 
-    
+    array = arr.sort 
+    left = 0
+    right = arr.length
+    mid = right/2
+    while left < right
+
+        sum = arr[mid] + arr[mid-1]
+        if sum > target_sum 
+            right = arr[0..mid].length
+            mid = right/2
+        elsif sum < target_sum
+           if target > arr[0] + arr[-1] 
+              arr =  arr[left+1..mid]
+              left += 1
+           elsif 
+            
+
+        #    arr[0] + arr[-1] < target_sum
+        #    arr[1..-1]
+           break
+        end
+
+    end
+    arr.each    
 end
+[1,    2, 3 ,4   ,5,    6   ,7  ,8,  9, 10, 11, 12]
+
+
+md = arr.length/2 
+arr[md] + arr[md-1] < target
+new_arr = arr[0..md]
