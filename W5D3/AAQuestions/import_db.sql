974c4dcfce1f4d6e28d170314dcec69240b22738
lpoole <keithpoole123@gmail.com>
2021-02-17 17:33:20 -0500

    import_db created

--- W5D3/AAQuestions/import_db.sql
+++ W5D3/AAQuestions/import_db.sql
@@ -18,8 +18,32 @@ CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NUll,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
-CREATE TABLES replies (
-    
-)
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_reply INTEGER,
+    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (parent_reply) REFERENCES replies(id), 
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  likes INTEGER,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname,lname)
+VALUES
+  ('Frank', 'Mendez'), 
+  ('Lamar', 'Poole');
