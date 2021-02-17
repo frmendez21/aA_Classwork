2d1bc6ffbb5f31f088e1ed04754356fff6626af1
frmendez21 <frmendez209@gmail.com>
2021-02-17 14:10:29 -0800

    starting aa questions

---
+++ W5D3/AAQuestions/import_db.sql
@@ -0,0 +1,25 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NUll,
+);
+
+CREATE TABLES replies (
+    
+)
