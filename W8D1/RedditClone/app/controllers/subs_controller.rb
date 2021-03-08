6f495efd1a95ab70c6f3f3bc9cd51eab16b43079
theo m <theo@mantz.io>
2021-03-08 14:11:11 -0500

    created subs controller and views

--- W8D1/RedditClone/app/controllers/subs_controller.rb
+++ W8D1/RedditClone/app/controllers/subs_controller.rb
@@ -1,2 +1,57 @@
 class SubsController < ApplicationController
+
+    before_action :require_logged_in, except: [:index, :show]
+
+    before_action :require_moderator, only: [:edit, :update]
+
+
+    def new
+        @sub = Sub.new
+        render :new
+    end
+
+    def create
+        @sub = Sub.new(sub_params)
+        @sub.user_id = current_user.id
+        if @sub.save
+            redirect_to sub_url(@sub)
+        else
+            flash.now[:errors] = @sub.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @sub = Sub.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @sub = Sub.find_by(id: params[:id])
+        if @sub.update(sub_params)
+            redirect_to sub_url(@sub)
+        else
+            flash.now[:errors] = @sub.errors.full_messages
+            render :edit
+        end
+    end
+
+    def show
+        @sub = Sub.find_by(id: params[:id])
+        render :show
+    end
+
+    def index
+        @sub = Sub.all
+        render :index
+    end
+
+    private
+    def sub_params
+        params.require(:sub).permit(:title, :description)
+    end
+
+    def require_moderator
+        redirect_to subs_url unless current_user.id == self.moderator.id
+    end
 end
