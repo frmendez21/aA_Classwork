663d9e422b9c85c02a522e143b11d48d0e808e84
frmendez21 <frmendez209@gmail.com>
2021-03-11 10:05:17 -0800

    init

--- W8D3/skeleton 16/phase_4_recursion.js
+++ W8D3/skeleton 16/phase_4_recursion.js
@@ -77,31 +77,53 @@ const bsearch = (arr, target) => {
     }
     let mid = Math.floor(arr.length / 2);
     let value = arr[mid];
+    
     if (target === value) {
         return mid;
     } else if (target > value) {
-        if (bsearch(arr.slice(mid + 1, target) === -1)) {
-            return -1;
-        }
-        return mid + 1 + bsearch(arr.slice(mid + 1), target);
+        let right = bsearch(arr.slice(mid + 1), target)
+        return right === -1 ? -1 : mid + 1 + right;
     } else {
         return bsearch(arr.slice(0, mid), target);
     }
 }
-console.log(bsearch([1, 2, 3, 4, 5, 6], 7))
+// console.log(bsearch([1, 2, 3, 4, 5, 6], 7))
 
+const mergesort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    let sortedLeft = mergesort(left);
+    let sortedRight = mergesort(right);
+    return merge(sortedLeft, sortedRight)
+};
 
+const merge = (left, right) => {
+    let result = [];
+    while (left.length > 0 || right.length > 0) {
+        if (left[0] < right[0] || right == false) {
+            result.push(left.shift());
+        } else if (left[0] > right[0] || left == false) {
+            result.push(right.shift());
+        }
+    }
+    return result;
+};
 
-// if (arr.length === 0) {
-//         return -1;
-//     }
-//     let mid = Math.floor(arr.length / 2);
-//     let value = arr[mid];
-//     if (target === value) {
-//         return mid;
-//     } else if (target > value) {
-//         let right = bsearch(arr.slice(mid + 1), target)
-//         return right === -1 ? -1 : mid + 1 + right;
-//     } else {
-//         return bsearch(arr.slice(0, mid), target);
-//     }
+// console.log(mergesort([5, 3, 7, 2, -1, -9]))
+
+const subsets = (arr) => {
+    if (arr.length === 1) {
+        return arr;
+    };
+    let first = arr.shift()
+    let last = arr.pop()
+    arr.push(last, first)
+    return [first].concat([subsets(arr.slice(1))])
+};
+
+console.log(subsets([1, 2, 3]))
+
