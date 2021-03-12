1d349efd6d0f43474f8cb4d6448e095d90facb6a
heavyevy <everettsmith95@gmail.com>
2021-03-12 14:21:48 -0800

    Complete clock class and start addNumbers function

--- W8D5/callbackintro.js
+++ W8D5/callbackintro.js
@@ -1,3 +1,5 @@
+//CLOCK
+
 class Clock {
     constructor () {
         let date = new Date();  
@@ -5,7 +7,7 @@ class Clock {
         this.minutes = date.getMinutes();
         this.seconds = date.getSeconds();
         this.printTime();
-        setInterval(function() {
+        setInterval(() => {
             this._tick();
         },1000);
     }
@@ -14,9 +16,33 @@ class Clock {
         console.log(dateString);
     }
     _tick() {
-        if (this.seconds === 59) {
-            this.seconds = 00;
+        this.seconds += 1;
+        if (this.seconds === 60) {
+            this.seconds = 0;
             this.minutes += 1;
-        } 
+            if (this.minutes === 60) {
+                this.minutes = 0;
+                this.hours += 1;
+                this.hours %= 24;
+            }
+        }
+        this.printTime(); 
     }
 }
+
+// let clock = new Clock();
+
+// addNumbers
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    // it's okay if this part is magic; it just says that we want to
+    // 1. output the prompt to the standard output (console)
+    // 2. read input from the standard input (again, console)
+
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers()
