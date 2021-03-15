67d16d874c68dedc98ea78925480a255567ffc2a
MarioPOrlando <Orlandomario73@yahoo.com>
2021-03-15 16:38:49 -0700

    start of flappy bird

--- W9D1/skeleton 17/src/game.js
+++ W9D1/skeleton 17/src/game.js
@@ -1,6 +1,43 @@
+  const Level = require("./level")
+
+
+
+
 export default class FlappyBird {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
   }
+
+  animate(){
+  
+  };
+
+
+  restart(){
+    this.level = new Level(this.dimensions);
+    this.animate(this.ctx)
+  };
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
