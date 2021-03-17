df9a3bae5c590320bf5826374f7e418c56e788a1
jason <jmasters1000@yahoo.com>
2021-03-17 15:58:16 -0700

    Work on bindEvents, makeMove, and setupBoard functions

--- W9D3/JQTicTacToe/src/ttt-view.js
+++ W9D3/JQTicTacToe/src/ttt-view.js
@@ -2,18 +2,45 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.el = $el;
+    this.count = 0;
 
     this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.el.on("click", "li", (e) => {
+      let pos = $(e.target).attr("pos")
+      pos = pos.split(",").map(x => Number(x))
+      try {
+        $(e.target).append(this.game.currentPlayer)
+        this.game.playMove(pos)
+      } catch(err) {
+        alert(err.msg)
+        return
+      }
+      $(e.target).css("background-color", "white")
+      this.makeMove()
+    })
+  }
 
-  makeMove($square) {}
+  makeMove() {
+    let winner = this.game.winner()
+
+    if (this.game.isOver() && winner && this.count === 0) {
+      $(`<p>You win, ${winner}</p>`).appendTo($("ul"))
+      this.count += 1
+    }
+  }
 
   setupBoard() {
     let ul = $("<ul></ul>");
-    let lis = $("<li></li><li></li><li></li>");
-    ul.append(lis);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let li = $("<li id=square></li>").attr("pos", [i, j])
+        ul.append(li)
+      }
+    }
     this.el.append(ul)
   }
 }
