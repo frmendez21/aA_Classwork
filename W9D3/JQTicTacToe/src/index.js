04a01a708978101197aacb8ac2c8844aec06f6fc
frmendez21 <frmendez209@gmail.com>
2021-03-17 13:31:30 -0700

    working on ttt grid

--- W9D3/JQTicTacToe/src/index.js
+++ W9D3/JQTicTacToe/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view')
+const Game = require('./game')
 
   $(() => {
-    // Your code here
+    const el = $('.ttt')
+    const game = new Game();
+    new View(game, el);
   });
--- W9D3/JQTicTacToe/src/ttt-view.js
+++ W9D3/JQTicTacToe/src/ttt-view.js
@@ -1,11 +1,21 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.el = $el;
+
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let ul = $("<ul></ul>");
+    let lis = $("<li></li><li></li><li></li>");
+    ul.append(lis);
+    this.el.append(ul)
+  }
 }
 
 module.exports = View;
