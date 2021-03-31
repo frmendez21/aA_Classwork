951ad3ec77085f2a5998d5b7692a9a6fe0e9ca71
frmendez21 <frmendez209@gmail.com>
2021-03-31 14:06:24 -0700

    refactored code

---
+++ W11D3/skeleton-part-ii/frontend/components/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import PokemonIndexContainer from './../components/pokemon/pokemon_index_container';
+const App = () => (
+    <Route path="/" component={PokemonIndexContainer}/>
+)
+
+export default App;
