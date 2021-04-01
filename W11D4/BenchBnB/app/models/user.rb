9471daa4bacf253dae660c98784296dd19d87c84
frmendez21 <frmendez209@gmail.com>
2021-04-01 15:02:03 -0700

    authentication backend complete

---
+++ W11D4/BenchBnB/app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, :email, :session_token, presence: true, uniqueness:true
+    validates :password_digest, presence: true 
+    validates :password, length: { minimum: 1, allow_nil: true}
+
+    attr_reader :password 
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user 
+        else 
+            return nil 
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest) == password
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+
+end
