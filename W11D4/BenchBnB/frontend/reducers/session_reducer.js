db601804e9652874c240d49e214be6541f23be39
frmendez21 <frmendez209@gmail.com>
2021-04-02 09:56:02 -0700

    fixed bugs phase 1

--- W11D4/BenchBnB/frontend/reducers/session_reducer.js
+++ W11D4/BenchBnB/frontend/reducers/session_reducer.js
@@ -1,13 +1,12 @@
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
 
-const _nullSession = {currentUser: null}
+const _nullSession = {id: null}
 
 export default (state = _nullSession, action) => {
   Object.freeze(state)
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      debugger
-      return Object.assign({}, {currentUser: action.user})
+      return Object.assign({}, {id: action.user.id })
     case LOGOUT_CURRENT_USER:
       return _nullSession
     default:
