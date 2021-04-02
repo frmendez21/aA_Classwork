3169c9715b5fa4a8ced607db5ffb10c3ea7d7305
Justin Cheasty <cheasty@gmail.com>
2021-04-01 17:35:45 -0700

    finish phase 1

--- W11D4/BenchBnB/frontend/bench_bnb.jsx
+++ W11D4/BenchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import * as sessionAPI from "./util/session_api_util";
+import { login } from "./actions/session_actions"
+import configureStore from "./store/store"
+// import { receiveCurrentUser } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  const store = configureStore();
+  window.login = login;
+  // window.logout = logout;
+  // window.receiveCurrentUser = receiveCurrentUser
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
